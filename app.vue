c92efdbb1d645b430018fa534288dc9bcd415b8b
unknown <bensonagala@live.com>
2023-09-30 22:53:32 +0300

    project setup

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vue from '@vitejs/plugin-vue';
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
