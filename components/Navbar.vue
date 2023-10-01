8020bb7c406b579770aa71f6c7b6c22ec033788a
Foddie2 <Bensonagala@live.com>
2023-10-01 16:49:33 +0300

    Nav setup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-900">
+  <div class="bg-orange-200">
     <div
       class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"
     >
